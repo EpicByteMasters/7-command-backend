442f40f7c3367cc4a351641b50887bb2dd0b328c
Dmitrii <116075666+Samsooon@users.noreply.github.com>
2024-01-19 21:55:44 +0300

    added router to app.api.__init__.py

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.endpoints.notifications import router as notifications_router
+from . import notifications_router
 
 main_router = APIRouter(prefix='/api/v1')
 main_router.include_router(notifications_router)
