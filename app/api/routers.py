04d567c71c6fadef21675084092301ffbc6e65b6
pgorshkova <polina_gorshkova_0202@mail.ru>
2024-01-22 21:23:31 +0300

    auth

--- app/api/routers.py
+++ app/api/routers.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from . import notifications_router
 from . import ipr_router
+from . import user_router
 
 
-main_router = APIRouter(prefix='/api/v1')
+main_router = APIRouter(prefix="/api/v1")
 main_router.include_router(ipr_router)
 main_router.include_router(notifications_router)
+main_router.include_router(user_router)
