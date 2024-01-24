ecd5efd6df6990711709cc3f5f6d8b880b488775
pgorshkova <polina_gorshkova_0202@mail.ru>
2024-01-24 17:46:12 +0300

    БД заполнена

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,9 +1,23 @@
+from fastapi import Depends
 from fastapi.routing import APIRouter
-from app.core.user import auth_backend, fastapi_users
+from sqlalchemy import select
 
+from app.core.db import AsyncSessionLocal, get_async_session
+from app.core.user import auth_backend, fastapi_users
+from app.models import User
 from app.schemas.user import UserRead, UserCreate
 
 router = APIRouter()
+
+
+@router.get("/user/{id}", response_model=UserRead)
+async def read_users(id: int, session: AsyncSessionLocal = Depends(get_async_session)):
+    # Получить пользователя из базы данных по идентификатору
+    user = await session.execute(select(User).filter(User.id == id))
+    user = user.scalar_one()
+
+    return user
+
 
 router.include_router(
     fastapi_users.get_auth_router(auth_backend),
