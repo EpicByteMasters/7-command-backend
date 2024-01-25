c6aa9b7178860d9a1cfd90f4683498cc97345f2f
Sobiyk <kirill.sobkovskii@yandex.ru>
2024-01-25 16:21:49 +0300

    Отключили нотификации на время

--- app/api/endpoints/notifications.py
+++ app/api/endpoints/notifications.py
@@ -1,10 +1,9 @@
-# from typing import List
-
 from fastapi import Depends, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
 from app.core.user import current_user
+
 # from app.crud.notifications import check_notifications, get_user_notifications
 from app.models import User
 from app.schemas.notifications import NotificationGet
@@ -12,12 +11,18 @@ from app.schemas.notifications import NotificationGet
 router = APIRouter()
 
 
-@router.get("/notifications",
-            response_model=List[NotificationGet],
-            dependencies=[Depends(current_user)])
-async def get_notifications(user: User = Depends(current_user),
-                            session: AsyncSession = Depends(get_async_session)):
+@router.get(
+    "/notifications",
+    response_model=list[NotificationGet],
+    dependencies=[Depends(current_user)],
+)
+async def get_notifications(
+    user: User = Depends(current_user),
+    session: AsyncSession = Depends(get_async_session),
+):
     pass
+
+
 #     await check_notifications(user, session)
 #     notifications = await get_user_notifications(user, session)
 #     return notifications
