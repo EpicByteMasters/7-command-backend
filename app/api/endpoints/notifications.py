356eb77ced68039816ab8738626c719f6ee95d9b
NikolayRudakov <nrudakov@yandex.ru>
2024-01-25 12:41:24 +0300

    Commit for workflow testing (Nothing changed)

--- app/api/endpoints/notifications.py
+++ app/api/endpoints/notifications.py
@@ -17,9 +17,7 @@ user_check_status = {}
 
 
 @router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket,
-                             user: User = Depends(current_user)
-                             ):
+async def websocket_endpoint(websocket: WebSocket, user: User = Depends(current_user)):
     await websocket.accept()
 
     try:
@@ -27,17 +25,18 @@ async def websocket_endpoint(websocket: WebSocket,
             data = await websocket.receive_text()
             print(f"Received message from {user.id}: {data}")
 
-            user_check_status[current_user.id] = True if data.lower() == "start" else False
+            user_check_status[current_user.id] = (
+                True if data.lower() == "start" else False
+            )
     finally:
         del user_websockets[current_user.id]
 
 
-@router.get("/notifications",
-            response_class=StreamingResponse
-            )
-async def notifications_endpoint(user: User = Depends(current_user),
-                                 session: AsyncSession = Depends(get_async_session)
-                                 ):
+@router.get("/notifications", response_class=StreamingResponse)
+async def notifications_endpoint(
+    user: User = Depends(current_user),
+    session: AsyncSession = Depends(get_async_session),
+):
     async def generate():
         while True:
             await asyncio.sleep(60)
