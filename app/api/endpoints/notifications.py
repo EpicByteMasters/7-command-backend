5dacd094bd8fed354dec4fc3f45ea08f89fd82d9
Dmitrii <116075666+Samsooon@users.noreply.github.com>
2024-01-25 19:53:35 +0300

    check

--- app/api/endpoints/notifications.py
+++ app/api/endpoints/notifications.py
@@ -1,17 +1,21 @@
-from typing import List
-
 from fastapi import Depends, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
 from app.core.user import current_user
+<<<<<<< HEAD
 from app.crud.notifications import get_user_notifications
+=======
+
+# from app.crud.notifications import check_notifications, get_user_notifications
+>>>>>>> develop
 from app.models import User
 from app.schemas.notifications import NotificationGet
 
 router = APIRouter()
 
 
+<<<<<<< HEAD
 @router.get("/notifications",
             response_model=List[NotificationGet],
             dependencies=[Depends(current_user)])
@@ -19,3 +23,20 @@ async def get_notifications(user: User = Depends(current_user),
                             session: AsyncSession = Depends(get_async_session)):
     notifications = await get_user_notifications(user, session)
     return notifications
+=======
+@router.get(
+    "/notifications",
+    response_model=list[NotificationGet],
+    dependencies=[Depends(current_user)],
+)
+async def get_notifications(
+    user: User = Depends(current_user),
+    session: AsyncSession = Depends(get_async_session),
+):
+    pass
+
+
+#     await check_notifications(user, session)
+#     notifications = await get_user_notifications(user, session)
+#     return notifications
+>>>>>>> develop
