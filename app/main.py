3695294c16114aeb3361ae0a4012a8d0ca4e80db
pgorshkova <polina_gorshkova_0202@mail.ru>
2024-01-26 21:47:35 +0300

    CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routers import main_router
 from app.core.config import settings
@@ -10,3 +11,16 @@ app = FastAPI(
 )
 
 app.include_router(main_router)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
