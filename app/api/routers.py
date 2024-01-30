fbe5cdc545510879984fa49db344a6122e0e07fd
NikolayRudakov <nrudakov@yandex.ru>
2024-01-30 19:06:08 +0300

    working 2

--- app/api/routers.py
+++ app/api/routers.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.api.endpoints import (
     auth_router,
     ipr_router,
+    iprs_router,
     notifications_router,
     secondary_router,
     task_router,
@@ -12,6 +13,7 @@ from app.api.endpoints import (
 main_router = APIRouter(prefix="/api/v1")
 main_router.include_router(auth_router, tags=["Авторизация и аутентификация"])
 main_router.include_router(ipr_router, prefix="/mentor/iprs/ipr")
+main_router.include_router(iprs_router, prefix="/mentor/iprs", tags=["План развития"])
 main_router.include_router(notifications_router, tags=["Уведомления"])
 main_router.include_router(user_router, tags=["Пользователи"])
 main_router.include_router(
